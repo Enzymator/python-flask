286fdcf697a938f85b0f2f27901fcd669b0ce400
Enzymator <delrue.pierre.dp@gmail.com>
2021-08-04 16:25:13 +0200

    Modification structure pour projet

--- app/app.py
+++ app/app.py
@@ -6,37 +6,37 @@ from werkzeug.utils import redirect
 app=Flask(__name__)
 utilisateur=UtilisateurModel()
 
-@app.route("/")
+@app.route("/app/")
 def hello():
     result=utilisateur.fetch_all_utilisateur()
-    return render_template("./Hello.html", data=result)
+    return render_template("./app/Hello.html", data=result)
 
-@app.route("/bye/")
+@app.route("/app/bye/")
 def bye():
-    return render_template("bye.html")
+    return render_template("./app/bye.html")
 
-@app.route("/add_user/")
+@app.route("/app/add_user/")
 def add_user():
-    return render_template("add_user.html")
+    return render_template("./app/add_user.html")
 
-@app.route("/insert_user/", methods=["POST", "GET"])
+@app.route("/app/insert_user/", methods=["POST", "GET"])
 def user_continent():
     data=request.form
     utilisateur.addUser(data)
-    return redirect("http://localhost:5000")
+    return redirect("http://localhost/app/")
 
-@app.route("/delete_user/<id>")
+@app.route("/app/delete_user/<id>")
 def delete_user(id):
     utilisateur.deleteById(id)
-    return redirect("http://localhost:5000")
+    return redirect("http://localhost/app/")
 
-@app.route("/update_user/")
+@app.route("/app/update_user/")
 def update_user():
     data1=request.args
-    return render_template(f"update_user.html",data=data1)
+    return render_template(f"./app/update_user.html",data=data1)
 
-@app.route("/update_user_t/", methods=["POST", "GET"])
+@app.route("/app/update_user_t/", methods=["POST", "GET"])
 def update_user_t():
     data=request.form
     utilisateur.update(data)
-    return redirect("http://localhost:5000")
+    return redirect("http://localhost/app/")
